051aaf149c3eb3d008375760d2d8c5df4a6b5a22
gulbod <118349125+gulbod@users.noreply.github.com>
2026-04-16 21:59:59 +0300

    Добавьте файлы проекта.

---
+++ JarBird/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarBird
+{
+    internal static class Core
+    {
+        public static JarBirdEnitities Context = new JarBirdEnitities();
+        public static Users AuthUser = null;
+    }
+}
---
+++ JarBird/Pages/ProductsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JarBird.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductsPage.xaml
+    /// </summary>
+    public partial class ProductsPage : Page
+    {
+        public ProductsPage()
+        {
+            InitializeComponent();
+            ListBoxProducts.ItemsSource = Core.Context.Products.ToList();
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                ListBoxProducts.ItemsSource = Core.Context.Products.ToList();
+            }
+        }
+    }
+}
